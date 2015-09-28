db28849ce5439de501cf88a8d6ab688134aba64a
NancyKo <konancys@gmail.com>
2015-09-27 21:28:52 -0400

    done

--- js/selectors.js
+++ js/selectors.js
@@ -8,3 +8,24 @@ $(document).ready(function(){
 });
 
 // declare your functions here...
+
+  function h1Selector() {
+   return $('h1');
+  }
+
+  function liInOlSelector() {
+  // Write a function that selects all <li> inside of <ol>.
+    return $('ol li');    
+  }
+
+  function linkSelector() {
+    return $('#box4.box5 a');
+  }
+
+  function imageSelector() {
+    return $("img[alt*='cat sleeping']");
+  }
+
+  function checkboxInputSelector() {
+    return $("input[type='checkbox']").wrap("<span class='pinky'></span");
+  }
